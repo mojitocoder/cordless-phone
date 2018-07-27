8e36c145cd03c2724aacee49225efbcc3ecc41f5
Quynh Nguyen <quynhnh@gmail.com>
2018-07-27 22:48:51 +0100

    Add dial and hangup

--- handler.js
+++ handler.js
@@ -4,6 +4,8 @@ const accountSid = process.env.account_sid;
 const authToken = process.env.auth_token;
 const fromNumber = process.env.from_number;
 
+const twimlDial = 'https://handler.twilio.com/twiml/EH3246f5c10b1c6bfb6388cf682d1e94f8'
+
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 const ClientCapability = require('twilio').jwt.ClientCapability;
@@ -54,4 +56,30 @@ module.exports.token = async (event, context) => {
   };
 
   return response;
+};
+
+module.exports.dial = async (event, context) => {
+  const toAgent = "client:" + event.queryStringParameters.username;
+  const url = twimlDial + '?phone_number=' + event.queryStringParameters.phone_number;
+
+  const call = await client.calls.create({
+    url: url,
+    to: toAgent,
+    from: fromNumber
+  });
+
+  const response = {
+    statusCode: 200
+  };
+
+  return response;
+};
+
+module.exports.hangup = async (event, context) => {
+  const callSid = event.queryStringParameters.call_sid;
+  const call = await client.calls(callSid).update({status: 'completed'});
+
+  return {
+    statusCode: 200
+  };
 };
